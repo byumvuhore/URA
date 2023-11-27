3c87c0e0dc85588bfa0c551165af1f29630838b5
byumvuhore <byumvuhorelucien@gmail.com>
2023-11-27 09:04:29 -0800

    Final Exam

--- models.js
+++ models.js
@@ -1,1 +1,78 @@
-//For models
+//For modelsconst mongoose = require('mongoose');
+
+// Taxpayer model schema
+const taxpayerSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: Date,
+    occupation: String,
+    gender: {
+      type: String,
+      enum: ['Male', 'Female'],
+    },
+    phoneNumber: String,
+    emailAddress: String,
+    expectedAnnualIncome: Number,
+    tin: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  });
+  
+  const Taxpayer = mongoose.model('Taxpayer', taxpayerSchema);
+  
+  // Asset model schema
+  const assetSchema = new mongoose.Schema({
+    assetName: {
+      type: String,
+      required: true,
+    },
+    estimatedCost: Number,
+    ownerTIN: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['Fixed', 'Movable'],
+    },
+    assetNumber: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  });
+  
+  const Asset = mongoose.model('Asset', assetSchema);
+  
+  // IncomeTaxPayment model schema
+  const incomeTaxPaymentSchema = new mongoose.Schema({
+    payerTIN: {
+      type: String,
+      required: true,
+    },
+    profits: {
+      type: Number,
+      required: true,
+    },
+    amountPaid: {
+      type: Number,
+      required: true,
+    },
+    paymentDate: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+  
+  const IncomeTaxPayment = mongoose.model('IncomeTaxPayment', incomeTaxPaymentSchema);
+  
+  module.exports = {
+    Taxpayer,
+    Asset,
+    IncomeTaxPayment,
+  };
+  
--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-//Express server here.
+//Express server here.const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { Taxpayer, Asset, IncomeTaxPayment } = require('./model'); // Make sure to adjust the path based on your file structure
+
+const app = express();
+const port = 3000; // Use the desired port
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://byumvuhorelucien:byumv2008@cluster0.8p6bbyo.mongodb.net/URA?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middleware for parsing JSON requests
+app.use(bodyParser.json());
+
+// Route to capture and save data for registering a tax payer
+app.post('/register-taxpayer', async (req, res) => {
+  try {
+    const taxpayerData = req.body;
+    const newTaxpayer = await Taxpayer.create(taxpayerData);
+    res.json(newTaxpayer);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to fetch all registered taxpayers
+app.get('/taxpayers', async (req, res) => {
+  try {
+    const taxpayers = await Taxpayer.find();
+    res.json(taxpayers);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Similar routes can be created for asset registration, income tax payment, and asset transfer
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
